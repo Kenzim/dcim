f33d888f27cce258ddd71fb475e239f47602f5ef
Kenzi Marcel <kenzi@kenzim.co.uk>
2026-01-17 17:21:04 +0000

    Update models __init__ to export new models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,8 +4,10 @@ from app.models.server import Server
 from app.models.category import Category
 from app.models.location import Location
 from app.models.disk import Disk, DiskType
+from app.models.boot_task import BootTask, BootType, BootTaskStatus
+from app.models.installation_task import InstallationTask, InstallationStatus
 # Import junction table to ensure it's registered with SQLAlchemy
 from app.models.plugin_category import plugin_categories
 
-__all__ = ["User", "Plugin", "Server", "Category", "Location", "Disk", "DiskType", "plugin_categories"]
+__all__ = ["User", "Plugin", "Server", "Category", "Location", "Disk", "DiskType", "BootTask", "BootType", "BootTaskStatus", "InstallationTask", "InstallationStatus", "plugin_categories"]
 
